134f59cb14931fd035bb24d12b2772e656b3d4af
Valkyrja <kilyanet@gmail.com>
2021-03-01 22:55:50 +0300

    added AttachmentsHelper

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,15 +2,18 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true));
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.startMaximized = true;
 
@@ -21,13 +24,13 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub";
     }
 
-    /*@AfterEach
+    @AfterEach
     public void afterEach() {
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
         if(System.getProperty("video_storage") != null)
             attachVideo();
-        closeWebDriver();
-    } */
+        //closeWebDriver();
+    }
 }
